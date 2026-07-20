c4f785d300725d0700ebfc444a0f1b985e89681e
ridham2007 <ridhampatel2907@gmail.com>
2026-07-20 21:38:37 +0530

    creation and insertion of data

---
+++ Lecture-01/practice1.sql
@@ -0,0 +1,107 @@
+create database dealership;
+USE dealership;
+create table suzuki (
+		car_id INT PRIMARY KEY,
+        car_name VARCHAR(20),
+        model VARCHAR(10),
+        fuel VARCHAR(10),
+        colour VARCHAR(10),
+        transmission VARCHAR(10),
+        price_lakh DECIMAL(6,2)
+        );
+select * from suzuki;
+create table employees(
+				e_id INT PRIMARY KEY,
+                e_name VARCHAR(15),
+                e_city VARCHAR(5),
+                e_phone VARCHAR(10)
+                );
+select * from employees;
+create table customers(
+				c_id INT PRIMARY KEY,
+                c_name VARCHAR(10),
+                c_city VARCHAR(10),
+                car_id INT
+                );
+select * from customers;
+create table sale(
+					sale_id INT PRIMARY KEY,
+					car_id INT,
+                    e_id INT,
+                    c_id INT
+                    );
+select * from sales;
+
+insert into suzuki
+			  values(1,'baleno','zeta','petrol','white','auto',13.00);
+insert into suzuki
+				values (2,'Baleno','Delta','Petrol','Blue','Manual',9.20),
+					(3,'Baleno','Alpha','Petrol','Grey','Manual',11.20),
+					(4,'Baleno','Zeta','Petrol','Black','Auto',13.00),
+					(5,'Swift','Delta','Petrol','White','Manual',8.00),
+					(6,'Swift','Alpha','Petrol','Red','Manual',9.40),
+					(7,'Swift','Zeta','Petrol','Blue','Auto',10.80),
+					(8,'Dzire','Delta','Petrol','Silver','Manual',8.80),
+					(9,'Dzire','Alpha','Petrol','White','Manual',10.20),
+					(10,'Dzire','Zeta','Petrol','Black','Auto',11.80),
+					(11,'Brezza','Delta','Petrol','Grey','Manual',10.50),
+					(12,'Brezza','Alpha','Petrol','Blue','Manual',12.40),
+					(13,'Brezza','Zeta','Petrol','White','Auto',14.20),
+					(14,'Fronx','Delta','Petrol','Red','Manual',9.80),
+					(15,'Fronx','Alpha','Petrol','Black','Manual',11.70),
+					(16,'Fronx','Zeta','Petrol','Silver','Auto',13.60),
+					(17,'Ertiga','Delta','Petrol','White','Manual',10.80),
+					(18,'Ertiga','Alpha','Petrol','Grey','Manual',12.60),
+					(19,'Ertiga','Zeta','Petrol','Blue','Auto',14.80),
+					(20,'Grand Vitara','Delta','Petrol','Black','Manual',14.50),
+					(21,'Grand Vitara','Alpha','Petrol','White','Auto',16.80),
+					(22,'Grand Vitara','Zeta','Petrol','Blue','Auto',19.20);
+select * from suzuki;
+
+insert into employees 
+		values	(1,'Raj','AMD','9876543210'),
+				(2,'Soham','RJT','9876543211'),
+				(3,'Veena','SNR','9876543212'),
+				(4,'Amit','JAM','9876543213'),
+				(5,'Priya','SUR','9876543214'),
+				(6,'Riya','GNR','9876543215'),
+				(7,'Karan','AMD','9876543216'),
+				(8,'Neha','BRD','9876543217');
+select * from employees;
+
+insert into customers
+					VALUES	(1,'Rahul','AMD',2),
+							(2,'Pooja','RJT',4),
+							(3,'Kunal','SNR',5),
+							(4,'Sneha','SUR',7),
+							(5,'Yash','JAM',9),
+							(6,'Nidhi','AMD',10),
+							(7,'Harsh','BRD',11),
+							(8,'Komal','GNR',13),
+							(9,'Vivek','RJT',14),
+							(10,'Meera','SNR',16),
+							(11,'Rohan','SUR',17),
+							(12,'Isha','AMD',18),
+							(13,'Dhruv','JAM',20),
+							(14,'Krupa','BRD',21),
+							(15,'Ankit','GNR',22);
+select * from customers;
+
+INSERT INTO sale VALUES
+			(1,2,1,1),
+			(2,4,2,2),
+			(3,5,3,3),
+			(4,7,4,4),
+			(5,9,5,5),
+			(6,10,6,6),
+			(7,11,7,7),
+			(8,13,8,8),
+			(9,14,1,9),
+			(10,16,2,10),
+			(11,17,3,11),
+			(12,18,4,12),
+			(13,20,5,13),
+			(14,21,6,14),
+			(15,22,7,15);
+select * from sale;
+drop table sales;
